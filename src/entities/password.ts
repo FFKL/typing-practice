b8b631789ec1574f2dd862d80d79685a7cb85fd2
dmitrii <ffklffkl@gmail.com>
2021-03-16 00:05:30 +0200

    rework password length check

--- src/entities/password.ts
+++ src/entities/password.ts
@@ -1,9 +1,7 @@
 import * as t from "runtypes";
 
-const PASSWORD_PATTERN = /^\w{4,}$/;
-
 export const Password = t.String
-  .withConstraint(s => new RegExp(PASSWORD_PATTERN).test(s) || `'${s}' is not a valid password`, { name: 'ValidPassword' })
+  .withConstraint(s => s.length >= 4 || `'${s}' is not a valid password`, { name: 'ValidPassword' })
   .withBrand('Password');
 
 export type Password = t.Static<typeof Password>;
--- src/services/login-service.ts
+++ src/services/login-service.ts
@@ -6,7 +6,7 @@ import UserService from './user-service';
 export default class LoginService {
   constructor(private readonly userService: UserService) { }
 
-  public async  login(email: Email, password: Password): Promise<User> {
+  public async login(email: Email, password: Password): Promise<User> {
     const foundUser = await this.userService.findUser(email, password);
     this.assertUserExistence(foundUser);
 
